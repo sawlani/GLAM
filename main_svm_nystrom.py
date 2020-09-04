1d6ce89adb8ee28432d5488bfb44d8e77dad4473
Saurabh Sawlani <sawlani@gatech.edu>
2020-09-04 18:20:55 -0400

    supervised with nystrom - inefficient version

--- main_svm_nystrom.py
+++ main_svm_nystrom.py
@@ -17,37 +17,38 @@ from models.svm import SVM
 import os
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 
-def train(args, model, svm, device, train_graphs, model_optimizer, svm_optimizer, epoch, k = 40):
+def train(args, model, svm, device, train_graphs, model_optimizer, svm_optimizer, epoch, k, layer="all"):
     model.train()
     
-    N = len(train_graphs)
-    
-    a = list(range(N))
-    np.random.shuffle(a)
-    Z_index = a[:k]
-    Z = [train_graphs[i] for i in Z_index]
+    Z = np.random.permutation(train_graphs)[:k]
 
-    Z_embeddings = model(Z, 1)
+    Z_embeddings = model(Z, layer)
     
+    # compute gamma
     all_vertex_embeddings = torch.cat(Z_embeddings, axis=0).detach()
     gamma = 1/torch.median(torch.cdist(all_vertex_embeddings, all_vertex_embeddings)**2)
+    
+    # compute kernel of landmark graphs
     K_Z = compute_mmd_gram_matrix(Z_embeddings, gamma=gamma)
+    
     eigenvalues, U_Z = torch.symeig(K_Z, eigenvectors=True)
     T = torch.matmul(U_Z,torch.diag(eigenvalues**-0.5))
 
     
-    dists_batch_list = []
-
     loss_accum = 0
 
     total_iters = args.iters_per_epoch
     pbar = tqdm(range(total_iters), unit='batch')
 
+    model_optimizer.zero_grad()
+        
+
     for pos in pbar:
         selected_idx = np.random.permutation(len(train_graphs))[:args.batch_size]
         batch_graph = [train_graphs[idx] for idx in selected_idx]
         
-        R_embeddings = model(batch_graph,1)
+        R_embeddings = model(batch_graph,layer)
+
         K_RZ = compute_mmd_gram_matrix(R_embeddings, Z_embeddings, gamma=gamma)
         F = torch.matmul(K_RZ, T)
         
@@ -61,17 +62,84 @@ def train(args, model, svm, device, train_graphs, model_optimizer, svm_optimizer
 
         #backprop
         svm_optimizer.zero_grad()
-        model_optimizer.zero_grad()
-        
-        loss.backward()
-        
-        svm_optimizer.step()
-        model_optimizer.step()
+    
+        loss.backward(retain_graph=True)
 
+        svm_optimizer.step()
+    
+
+        R_embeddings = None
+        
         loss = loss.detach().cpu().numpy()
         loss_accum += loss
 
         pbar.set_description('epoch: %d' % (epoch))
+
+    model_optimizer.step()
+
+        
+    average_loss = loss_accum/total_iters
+    
+    return average_loss
+
+
+def train2(args, model, svm, device, train_graphs, model_optimizer, svm_optimizer, epoch, k, layer="all"):
+    model.train()
+    
+    loss_accum = 0
+
+    total_iters = args.iters_per_epoch
+    pbar = tqdm(range(total_iters), unit='batch')
+
+    for pos in pbar:
+        selected_idx = np.random.permutation(len(train_graphs))[:args.batch_size]
+        batch_graph = [train_graphs[idx] for idx in selected_idx]
+
+        Z = np.random.permutation(train_graphs)[:k]
+
+        Z_embeddings = model(Z, layer)
+        
+        # compute gamma
+        all_vertex_embeddings = torch.cat(Z_embeddings, axis=0).detach()
+        gamma = 1/torch.median(torch.cdist(all_vertex_embeddings, all_vertex_embeddings)**2)
+        
+        # compute kernel of landmark graphs
+        K_Z = compute_mmd_gram_matrix(Z_embeddings, gamma=gamma)
+        
+        eigenvalues, U_Z = torch.symeig(K_Z, eigenvectors=True)
+        T = torch.matmul(U_Z,torch.diag(eigenvalues**-0.5))
+
+        
+        R_embeddings = model(batch_graph,layer)
+
+        K_RZ = compute_mmd_gram_matrix(R_embeddings, Z_embeddings, gamma=gamma)
+        F = torch.matmul(K_RZ, T)
+        
+        output = svm(F).flatten()
+
+        labels = torch.LongTensor([graph.label for graph in batch_graph]).to(device)
+        labels[labels == 0] = -1  # Replace zeros with -1
+        
+        losses = torch.clamp(1 - output * labels, min=0) # hinge loss (unregularized)
+        loss = torch.mean(losses)
+
+        #backprop
+        model_optimizer.zero_grad()
+        svm_optimizer.zero_grad()
+    
+        loss.backward()
+
+        svm_optimizer.step()
+        model_optimizer.step()
+
+        R_embeddings = None
+        
+        loss = loss.detach().cpu().numpy()
+        loss_accum += loss
+
+        pbar.set_description('epoch: %d' % (epoch))
+
+    
         
     average_loss = loss_accum/total_iters
     
@@ -79,21 +147,20 @@ def train(args, model, svm, device, train_graphs, model_optimizer, svm_optimizer
 
 
 ###pass data to model with minibatch during testing to avoid memory overflow (does not perform backpropagation)
-def pass_data_iteratively(model, svm, graphs, Z_embeddings, T, gamma, minibatch_size = 64):
-    model.eval()
+def pass_data_iteratively(model, svm, graphs, Z_embeddings, T, gamma, layer, minibatch_size = 32):
     output = []
     idx = np.arange(len(graphs))
     for i in range(0, len(graphs), minibatch_size):
         sampled_idx = idx[i:i+minibatch_size]
         if len(sampled_idx) == 0:
             continue
-        embeddings = model([graphs[j] for j in sampled_idx], 1)
+        embeddings = model([graphs[j] for j in sampled_idx], layer)
         K_RZ = compute_mmd_gram_matrix(embeddings, Z_embeddings, gamma=gamma)
         F = torch.matmul(K_RZ, T)
         output.append(svm(F).flatten())
     return torch.cat(output, 0)
 
-def test(args, model, svm, device, test_graphs, k):
+def test(args, model, svm, device, test_graphs, k, layer="all"):
     model.eval()
 
     N = len(test_graphs)
@@ -103,21 +170,23 @@ def test(args, model, svm, device, test_graphs, k):
     Z_index = a[:k]
     Z = [test_graphs[i] for i in Z_index]
 
-    Z_embeddings = model(Z, 1)
+    Z_embeddings = model(Z, layer)
     
     all_vertex_embeddings = torch.cat(Z_embeddings, axis=0).detach()
     gamma = 1/torch.median(torch.cdist(all_vertex_embeddings, all_vertex_embeddings)**2)
+
     K_Z = compute_mmd_gram_matrix(Z_embeddings, gamma=gamma)
     eigenvalues, U_Z = torch.symeig(K_Z, eigenvectors=True)
     T = torch.matmul(U_Z,torch.diag(eigenvalues**-0.5))
 
     
-    output = pass_data_iteratively(model, svm, test_graphs, Z_embeddings, T, gamma)
+    output = pass_data_iteratively(model, svm, test_graphs, Z_embeddings, T, gamma, layer)
     
     pred = torch.sign(output)
     
     labels = torch.LongTensor([graph.label for graph in test_graphs]).to(device)
     labels[labels == 0] = -1
+    
     correct = pred.eq(labels.view_as(pred)).sum().cpu().item()
     acc_test = correct / float(len(test_graphs))
     
@@ -129,8 +198,8 @@ def main():
     parser = argparse.ArgumentParser(description='PyTorch GIN+MMD for whole-graph classification')
     parser.add_argument('--device', type=int, default=0,
                         help='which gpu to use if any (default: 0)')
-    parser.add_argument('--batch_size', type=int, default=10,
-                        help='input batch size for training (default: 10)')
+    parser.add_argument('--batch_size', type=int, default=32,
+                        help='input batch size for training (default: 32)')
     parser.add_argument('--iters_per_epoch', type=int, default=50,
                         help='number of iterations per each epoch (default: 50)')
     parser.add_argument('--epochs', type=int, default=50,
@@ -151,8 +220,12 @@ def main():
                                         help='Whether to learn the epsilon weighting for the center nodes. Does not affect training accuracy though.')
     parser.add_argument('--degree_as_tag', action="store_true",
     					help='let the input node features be the degree of nodes (heuristics for unlabeled graph)')
-    parser.add_argument('--dataset', type = str, default = "mixhop", choices=["mixhop", "chem"],
+    parser.add_argument('--dataset', type = str, default = "mixhop", choices=["mixhop", "chem", "contaminated"],
                                         help='dataset used')
+    parser.add_argument('--no_of_graphs', type = int, default = 100,
+                                        help='no of graphs generated')
+    parser.add_argument('--layer', type = str, default = "all",
+                                        help='which hidden layer used as embedding')
     args = parser.parse_args()
 
     #set up seeds and gpu device
@@ -163,18 +236,21 @@ def main():
         torch.cuda.manual_seed_all(0)
 
     if args.dataset == "mixhop":
-        graphs, num_classes = load_synthetic_data(number_of_graphs=400, h_inlier=0, h_outlier=1)
+        graphs, num_classes = load_synthetic_data(number_of_graphs=args.no_of_graphs, h_inlier=0.3, h_outlier=0.7)
         
     elif args.dataset == "contaminated":
-        graphs, num_classes = load_synthetic_data_contaminated(100)
+        graphs, num_classes = load_synthetic_data_contaminated(number_of_graphs=args.no_of_graphs)
     else:
         graphs, num_classes = load_chem_data()
 
     ##10-fold cross validation. Conduct an experiment on the fold specified by args.fold_idx.
     #train_graphs, test_graphs = separate_data(graphs,args.seed,args.fold_idx, 2)
-    train_graphs, test_graphs = graphs[:100] + graphs[300:], graphs[100:300]
+    graphs = np.random.permutation(graphs)
 
-    k=int(0.4*len(train_graphs))
+    train_graphs, test_graphs = graphs[:args.no_of_graphs//2], graphs[args.no_of_graphs//2:]
+    
+    k_frac = 0.4
+    k = int(k_frac*len(train_graphs))
     no_of_node_features = train_graphs[0].node_features.shape[1]
 
     model = GraphCNN_SVDD(args.num_layers, no_of_node_features, args.hidden_dim, num_classes, (not args.dont_learn_eps), args.neighbor_pooling_type, device).to(device)
@@ -187,7 +263,7 @@ def main():
 
     for epoch in range(1, args.epochs + 1):
         
-        avg_loss = train(args, model, svm, device, train_graphs, model_optimizer, svm_optimizer, epoch, k)
+        avg_loss = train2(args, model, svm, device, train_graphs, model_optimizer, svm_optimizer, epoch, k)
         print("Training loss: %f" % (avg_loss))
         
         #scheduler.step()
