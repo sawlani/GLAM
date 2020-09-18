6da5804a7238fa5fce69a42e169a3c9c9a2e8d43
Saurabh Sawlani <sawlani@gatech.edu>
2020-09-18 18:37:46 -0400

    vectorized gram matrix computation

--- mmd_util.py
+++ mmd_util.py
@@ -1,40 +1,89 @@
 import torch
-import numpy as np
-from sklearn import metrics
+from torch.nn.utils.rnn import pad_sequence
+
+def compute_gamma(embeddings):
+    all_vertex_embeddings = torch.cat(embeddings, axis=0).detach()
+    all_vertex_distances = torch.cdist(all_vertex_embeddings, all_vertex_embeddings)**2
+    median_of_distances = torch.median(all_vertex_distances)
+    if median_of_distances <= 1e-4:
+        median_of_distances = 1e-4
+    
+    gamma = 1/median_of_distances
+
+    return gamma
+
+def compute_mmd_gram_matrix(X_embeddings, Y_embeddings=None, gamma=None, type="SMM"):
+    
+    if not Y_embeddings:
+        Y_embeddings = X_embeddings
+
+    X_padded = pad_sequence(X_embeddings, batch_first=True)
+    Y_padded = pad_sequence(Y_embeddings, batch_first=True)
+
+    X_ones = [torch.ones(emb.shape[0]) for emb in X_embeddings]
+    Y_ones = [torch.ones(emb.shape[0]) for emb in Y_embeddings]
+
+    X_ones_padded = pad_sequence(X_ones, batch_first=True)
+    Y_ones_padded = pad_sequence(Y_ones, batch_first=True)
+    mask = X_ones_padded[:,None,:,None]*Y_ones_padded[None,:,None,:]
+
+    if gamma == None:
+        gamma = compute_gamma(X_embeddings)
+    if gamma==0:
+        print("zero gamma")
+
+    XY = torch.matmul(X_padded[:,None,:,:], torch.transpose(Y_padded[None,:,:,:], -1, -2))
+
+    if type=="SMM":
+        X_sq = torch.squeeze(torch.matmul(X_padded[:,:,None,:], X_padded[:,:,:,None]))
+        Y_sq = torch.squeeze(torch.matmul(Y_padded[:,:,None,:], Y_padded[:,:,:,None]))
+        
+        K_XY = torch.exp(-gamma * (-2 * XY + X_sq[:,None,:,None] + Y_sq[None,:,None,:]))
+
+        masked_means = torch.sum(K_XY*mask,(2,3))/torch.sum(mask,(2,3))
+    
+    return masked_means
 
 
-def rbf_mmd_old(X, Y, gamma=None, type="SMM"):
+
+
+''' Old code below - bot being used'''
+
+def rbf_mmd(X, Y, gamma=None, type="SMM"):
 
     if gamma == None:
         gamme = compute_gamma([X,Y])
     
-    XX = torch.mm(X, torch.transpose(X, 0, 1))
-    XY = torch.mm(X, torch.transpose(Y, 0, 1))
-    YY = torch.mm(Y, torch.transpose(Y, 0, 1))
-    X_sqnorms = torch.diagonal(XX)
-    Y_sqnorms = torch.diagonal(YY)
-
+    XY = torch.matmul(X, torch.transpose(Y, 0, 1))
     
     if type=="MMD":
-        K_XY = torch.exp(-gamma * (-2 * XY + X_sqnorms[:, np.newaxis] + Y_sqnorms[np.newaxis, :]))
-        K_XX = torch.exp(-gamma * (-2 * XX + X_sqnorms[:, np.newaxis] + X_sqnorms[np.newaxis, :]))
-        K_YY = torch.exp(-gamma * (-2 * YY + Y_sqnorms[:, np.newaxis] + Y_sqnorms[np.newaxis, :]))
+        XX = torch.matmul(X, torch.transpose(X, 0, 1))
+        YY = torch.matmul(Y, torch.transpose(Y, 0, 1))
+        X_sqnorms = torch.diagonal(XX)
+        Y_sqnorms = torch.diagonal(YY)
+
+        K_XY = torch.exp(-gamma * (-2 * XY + torch.unsqueeze(X_sqnorms,1) + torch.unsqueeze(Y_sqnorms,0)))
+        K_XX = torch.exp(-gamma * (-2 * XX + torch.unsqueeze(X_sqnorms,1) + torch.unsqueeze(X_sqnorms,0)))
+        K_YY = torch.exp(-gamma * (-2 * YY + torch.unsqueeze(Y_sqnorms,1) + torch.unsqueeze(Y_sqnorms,0)))
 
         output = K_XX.mean() + K_YY.mean() - 2 * K_XY.mean()
 
     elif type=="SMM":
-        K_XY = torch.exp(-gamma * (-2 * XY + X_sqnorms[:, np.newaxis] + Y_sqnorms[np.newaxis, :]))
+        X_sqnorms = torch.squeeze(torch.matmul(torch.unsqueeze(X,1),torch.unsqueeze(X,2)))
+        Y_sqnorms = torch.squeeze(torch.matmul(torch.unsqueeze(Y,1),torch.unsqueeze(Y,2)))
+        
+        K_XY = torch.exp(-gamma * (-2 * XY + torch.unsqueeze(X_sqnorms,1) + torch.unsqueeze(Y_sqnorms,0)))
         
         output = K_XY.mean()
 
     return output
     
-def rbf_mmd(X, Y, gamma):
+def rbf_mmd_simple(X, Y, gamma, type="SMM"):
     if gamma == None:
         gamme = compute_gamma([X,Y])
     return torch.mean(torch.exp(-1*gamma* torch.cdist(X,Y)**2))
 
-def compute_mmd_gram_matrix(X_embeddings, Y_embeddings=None, gamma=None, type="SMM"):
+def compute_mmd_gram_matrix_slow(X_embeddings, Y_embeddings=None, gamma=None, type="SMM"):
     
     if gamma == None:
         gamma = compute_gamma(X_embeddings)
@@ -48,7 +97,7 @@ def compute_mmd_gram_matrix(X_embeddings, Y_embeddings=None, gamma=None, type="S
         
         for i in range(n1):
             for j in range(n2):
-                MMD_values[i][j] = rbf_mmd_old(X_embeddings[i], Y_embeddings[j], gamma=gamma, type=type)
+                MMD_values[i][j] = rbf_mmd(X_embeddings[i], Y_embeddings[j], gamma=gamma, type=type)
     
     else:
         n = len(X_embeddings)
@@ -56,18 +105,7 @@ def compute_mmd_gram_matrix(X_embeddings, Y_embeddings=None, gamma=None, type="S
         
         for i in range(n):
             for j in range(i,n):
-                MMD_values[i][j] = rbf_mmd_old(X_embeddings[i], X_embeddings[j], gamma=gamma, type=type)
+                MMD_values[i][j] = rbf_mmd(X_embeddings[i], X_embeddings[j], gamma=gamma, type=type)
                 MMD_values[j][i] = MMD_values[i][j]
     
     return MMD_values
-
-def compute_gamma(embeddings):
-    all_vertex_embeddings = torch.cat(embeddings, axis=0).detach()
-    all_vertex_distances = torch.cdist(all_vertex_embeddings, all_vertex_embeddings)**2
-    median_of_distances = torch.median(all_vertex_distances)
-    if median_of_distances <= 1e-4:
-        median_of_distances = 1e-4
-    
-    gamma = 1/median_of_distances
-
-    return gamma
